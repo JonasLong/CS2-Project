faaeb3b2bd533c2420a11fa15dbaed1d2d9ba886
jl4231 <jl4231@rit.edu>
2022-04-12 18:43:38 -0400

    Started HoppersConfig

--- src/puzzles/hoppers/model/HoppersConfig.java
+++ src/puzzles/hoppers/model/HoppersConfig.java
@@ -2,7 +2,56 @@ package puzzles.hoppers.model;
 
 import puzzles.common.solver.Configuration;
 
-// TODO: implement your HoppersConfig for the common solver
+import java.util.ArrayList;
+import java.util.Collection;
 
-public class HoppersConfig {
+
+public class HoppersConfig implements Configuration {
+
+
+    private ArrayList<ArrayList<cellContents>> grid;
+    private static int ROWS;
+    private static int COLS;
+
+    public enum cellContents{
+        EMPTY, GREEN, RED, INVALID
+    }
+
+    @Override
+    public Collection<Configuration> getNeighbors() {
+        ArrayList<Configuration> neighbors = new ArrayList<>();
+
+        for (int row=0; row<ROWS; row++) {
+            for (int col=0; col<COLS; col++) {
+                if(isEven(col)){
+
+                }
+                //neighbors.add(new HoppersConfig());
+            }
+        }
+
+        return null;
+    }
+
+    private boolean isEven(int num){
+        return num%2==0;
+    }
+
+    public HoppersConfig(ArrayList<ArrayList<cellContents>> grid){
+        this.grid=grid;
+        ROWS=grid.size();
+        COLS=grid.get(0).size();
+    }
+
+    @Override
+    public boolean isSolution() {
+        for (ArrayList<cellContents> row: grid){
+            for (cellContents cell: row){
+                if(cell== cellContents.GREEN){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
