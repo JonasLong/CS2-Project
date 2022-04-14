bf11b7a91e3f4c5d706cda13284eb8f0b55e30a2
jl4231 <jl4231@rit.edu>
2022-04-14 15:39:59 -0400

    Thank you for making life less bad

--- src/puzzles/hoppers/ptui/HoppersPTUI.java
+++ src/puzzles/hoppers/ptui/HoppersPTUI.java
@@ -1,7 +1,12 @@
 package puzzles.hoppers.ptui;
 
 import puzzles.common.Observer;
+import puzzles.common.solver.Configuration;
+import puzzles.common.solver.Solver;
+import puzzles.hoppers.model.HoppersConfig;
 import puzzles.hoppers.model.HoppersModel;
+
+import java.io.IOException;
 
 public class HoppersPTUI implements Observer<HoppersModel, String> {
     private HoppersModel model;
@@ -13,6 +18,16 @@ public class HoppersPTUI implements Observer<HoppersModel, String> {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java HoppersPTUI filename");
+        } else {
+            String fname=args[0];
+            System.out.println("File: "+fname);
+            try {
+                Configuration c = new HoppersConfig(fname);
+                Solver s = new Solver();
+                s.findPath(c);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
